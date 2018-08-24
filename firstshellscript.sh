cbf23f61ee37550b20f175615a91c6fd605db837
vedavyas Burli <vedavyas.pb@gmail.com>
2018-08-24 23:10:10 +0530

    First shell script VED exit

---
+++ firstshellscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Welcome to Human bings arena"
+ACTION="EXITED"
+if [ "$ACTION" = "EXITED" ]
+then
+	echo "HURRRRRRY"
+	value=`expr 21 + 21 `
+	echo "value=$value"
+	exit 0
+else
+	echo "ok BYE"
+	exit 0
+fi
+exit 0
