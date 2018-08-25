cdaa1b645a17a18b5e68c4ce5147d8d90c019fc9
vedavyas Burli <vedavyas.pb@gmail.com>
2018-08-25 15:19:07 +0530

    modified while loop added

--- firstshellscript.sh
+++ firstshellscript.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 echo "Welcome to Human bings arena"
 ACTION="EXITED"
+sum=0
+while [ $sum -lt 10 ]
+do
 if [ "$ACTION" = "EXITED" ]
 then
 	echo "HURRRRRRY"
 	value=`expr 21 + 21 `
-	echo "value=$value"
+	echo "value=$value"	
 	exit 0
 else
 	echo "ok BYE"
 	exit 0
 fi
-exit 0
+sum=`expr $sum + 1`
+done
+
